c40d25c3d8dbb0300062cd37bfd271d75199ce13
Evan Conley <econmang@user.noreply.github.com>
2023-06-18 23:12:17 -0700

    Created visited check to stop loops

--- grokkingAlgos/ch6/impls/python/bfs.py
+++ grokkingAlgos/ch6/impls/python/bfs.py
@@ -10,13 +10,16 @@ class Graph:
     def bfs(self, start_node: str, end_node: str):
         search_queue = deque()
         search_queue += self.graph[start_node]
+        visited: list[str] = []
         while search_queue:
             curr_node = search_queue.popleft()
-            if curr_node == end_node:
-                print(f"Path to {end_node} was found!")
-                return True
-            else:
-                search_queue += self.graph[curr_node]
+            if not curr_node in visited:
+                if curr_node == end_node:
+                    print(f"Path to {end_node} was found!")
+                    return True
+                else:
+                    search_queue += self.graph[curr_node]
+                    visited.append(curr_node)
         print(f"No path to {end_node} was found!")
         return False
 
@@ -27,9 +30,10 @@ if __name__ == "__main__":
     graph_one.add_node("alice",  ["peggy"])
     graph_one.add_node("claire", ["thom","jonny"])
     graph_one.add_node("anuj",  [])
-    graph_one.add_node("peggy", [])
+    graph_one.add_node("peggy", ["you"])
     graph_one.add_node("thom",  [])
     graph_one.add_node("jonny", [])
 
     print(graph_one.bfs("you", "anuj"))
+    print(graph_one.bfs("you", "thom"))
     print(graph_one.bfs("you", "carl"))
